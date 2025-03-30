49c3127b1eea120d4362951be471199240f3546e
seth1525 <sethrollinsroblox@gmail.com>
2025-03-30 17:49:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def home():
     return "I'm alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
 
 def keep_alive():
     t = Thread(target=run)
